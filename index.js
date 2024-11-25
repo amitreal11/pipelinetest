23979ca27b5c57e634e318d7e617c16ecf02d629
aws amit <amitkatiyar@real11.com>
2024-11-25 17:19:50 +0530

    test2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import cors from 'cors'
 dotenv.config()
 const app = express()
 
-//========>test ======>1
+//========>test ======>2
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:false}))
