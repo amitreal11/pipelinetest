509bac3057f5609a1f2f3d3d826208d11a744621
amitreal11 <128671695+amitreal11@users.noreply.github.com>
2024-12-02 15:44:44 +0530

    Update appstart.sh
    chnges into file

--- scripts/appstart.sh
+++ scripts/appstart.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
- #npm start /home/ubuntu/node/server/server.js
 
-# cd ~/home/ubuntu/pipelinetest/scripts
+echo "Starting PM2 service restart" >> /tmp/codedeploy_debug.log
+cd /home/ubuntu/pipelinetest >> /tmp/codedeploy_debug.log 2>&1
+/home/ubuntu/.nvm/versions/node/v14.21.1/bin/pm2 restart 0 >> /tmp/codedeploy_debug.log 2>&1
+echo "PM2 service restarted successfully!" >> /tmp/codedeploy_debug.log
 
-pm2 restart 0
 
 
