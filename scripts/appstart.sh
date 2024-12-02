fccdd3fac842e1265b128fa5319f035aaf762abb
amitreal11 <128671695+amitreal11@users.noreply.github.com>
2024-12-02 16:07:28 +0530

    Update appstart.sh

--- scripts/appstart.sh
+++ scripts/appstart.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Load NVM (if using Node Version Manager)
-export NVM_DIR="/home/ubuntu/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# Navigate to the application directory
-cd /home/ubuntu/pipelinetest
-
-# Restart PM2 process as ubuntu
-pm2 restart app
+pm2 restart 0
 
 echo "PM2 service restarted successfully as ubuntu user!"
