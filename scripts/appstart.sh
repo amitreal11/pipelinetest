2b7b0bc8c508ac105a34b74623f6749a258350fa
amitreal11 <128671695+amitreal11@users.noreply.github.com>
2024-12-02 16:00:30 +0530

    Update appstart.sh

--- scripts/appstart.sh
+++ scripts/appstart.sh
@@ -2,7 +2,7 @@
 
 echo "Starting PM2 service restart" >> /tmp/codedeploy_debug.log
 cd /home/ubuntu/pipelinetest >> /tmp/codedeploy_debug.log 2>&1
-/home/ubuntu/.nvm/versions/node/v14.21.1/bin/pm2 restart 0 >> /tmp/codedeploy_debug.log 2>&1
+/home/ubuntu/.nvm/versions/node/v14.21.1/bin/pm2 restart node >> /tmp/codedeploy_debug.log 2>&1
 echo "PM2 service restarted successfully!" >> /tmp/codedeploy_debug.log
 
 
